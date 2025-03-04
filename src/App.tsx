be990ce6f3aa47210ee861d431e6b38d54d2dab2
Simón Sánchez <chivolennon@gmail.com>
2025-03-03 21:07:23 -0300

    react-router-dom / Login 0.1v

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 //Ahora se que puedo eliminar el import de react no es necesario.
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/LOG/Home";
+import { Login } from "./component/LOG/Login";
+import { Register } from "./component/LOG/Register";
+
 function App() {
   return (
-    <div>
-      <h2>Esto será el login.</h2>
+    <div className="bg-slate-300 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
